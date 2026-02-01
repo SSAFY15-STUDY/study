f2e436b6daedcc407a18a32834fc60a0cda84762
곽예경 <kyun9.k@gmail.com>
2026-02-01 21:06:41 +0900

    day1/test1,2: 2번째 풀이

--- day1/test2/test2_yekyung.java
+++ day1/test2/test2_yekyung.java
@@ -3,25 +3,24 @@ import java.util.Scanner;
 public class test2 {
 	public static void main(String[] args) {
 		int n = 5;
-		try (Scanner sc = new Scanner(System.in)) {
-			String line = sc.next();
-			int[] arr = new int[n];
-			
-			// sol1
+		Scanner sc = new Scanner(System.in);
+		String line = sc.next();
+		int[] arr = new int[n];
+
+		// sol1
 //			for (int i = 0; i < n; i++) {
 //				arr[i] = Character.getNumericValue(line.charAt(i));
 //			}
-			
-			// sol2
-			for (int i = 0; i < n; i++) {
-				arr[i] = line.charAt(i) - '0';
-			}
 
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < n; i++) {
-				sb.append(arr[i]).append(' ');
-			}
-			System.out.println(sb);
+		// sol2
+		for (int i = 0; i < n; i++) {
+			arr[i] = line.charAt(i) - '0';
 		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			sb.append(arr[i]).append(' ');
+		}
+		System.out.println(sb);
 	}
 }
